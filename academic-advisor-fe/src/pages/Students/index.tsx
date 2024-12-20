f2e52b32711dd9a02f108977062d3488c0958ba2
hoaibao2k4 <hoaibaodongthap@gmail.com>
2024-12-21 02:06:10 +0700

    based FE

---
+++ academic-advisor-fe/src/pages/Students/index.tsx
@@ -0,0 +1,28 @@
+import MUIDataTable from "mui-datatables";
+
+const columns = ["Student ID", "Name", "Class", "Major", "GPA", "Action", "Status", ];
+function Students(): JSX.Element {
+  const data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+  ];
+  const options = {
+    filterType: "checkbox",
+  };
+  return (
+    <div className="mt-10 mx-auto pb-4">
+      <div className="flex justify-center">
+        <MUIDataTable
+          title={"Employee List"}
+          data={data}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Students;
